e62989c467e779a9f60d67d66740e99a26bf4631
Tushar Nikam <nikamtushar46@gmail.com>
2025-08-15 00:38:32 +0530

    featuredsection update

--- client/src/Components/FeaturedSection.jsx
+++ client/src/Components/FeaturedSection.jsx
@@ -37,7 +37,7 @@ const FeaturedSection = () => {
 
       <div className="flex flex-wrap max-sm:justify-center gap-8 mt-8">
         {uniqueShows
-          .slice(0, 3)
+          .slice(0, 4)
           .map(
             (show) =>
               show.movie && (
